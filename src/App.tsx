2019cea984f33d0592ff59f2670372f33fd6f106
igor_alves <igornalves08@gmail.com>
2024-10-12 17:06:14 -0300

    Criando rotas para aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/Router";
 
 export default function App() {
   return (
-    <>
-        
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+
+      <GlobalStyles/>
+    </ThemeProvider>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,5 +6,4 @@ export const GlobalStyles = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
-    
 `
